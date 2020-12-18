0ad1ab3407a98009b6ec2b5a6d6cf80dd493cbcc
Ubuntu <ubuntu@ip-172-31-95-19.ec2.internal>
2020-12-18 16:12:22 +0000

    Added in game.c

---
+++ game.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Marcus A. Mosley All rights reserved.
+//
+// Created by Marcus A. Mosley
+// Created on December 2020
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+}
